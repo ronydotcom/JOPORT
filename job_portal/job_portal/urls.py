2327b0ead1a2d708163923605454d59938ef3c31
ronydotcom <r0083080@gmail.com>
2026-06-19 00:39:30 +0600

    final updated

--- job_portal/job_portal/urls.py
+++ job_portal/job_portal/urls.py
@@ -23,6 +23,6 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('prac_app.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
-
 ]
+urlpatterns+=re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+urlpatterns+=re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),

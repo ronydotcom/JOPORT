9d69080d952deb115fe7624b7bd7e4c17a5b2d39
ronydotcom <r0083080@gmail.com>
2026-06-19 15:50:20 +0600

    describe your changes

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ab+70m=@03x9#)yj*(qti7!qhpe6mr0&q0ji!ar$o(u30#(ly'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
